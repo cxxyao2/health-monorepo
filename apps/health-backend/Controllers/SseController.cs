ea6fd1c08860bfd410f6b1afb7bc41d5b35da0b6
jennifer <cxxyao2@gmail.com>
2024-09-28 00:49:51 -0400

    add Server sent event controller

---
+++ apps/health-backend/Controllers/SseController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace HealthBackend.Controllers
+{
+    [Route("[controller]")]
+    public class SseController : Controller
+    {
+        private readonly ILogger<SseController> _logger;
+
+        public SseController(ILogger<SseController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
