4f15f813d07812b7a00ede98d7193f800f007b21
jennifer <cxxyao2@gmail.com>
2024-09-28 21:03:01 -0400

    set modify=true  in tsconfig.base.json

--- apps/health-frontend/src/app/app.component.ts
+++ apps/health-frontend/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from '@health-frontend/app/nx-welcome.component';
-import { SseClientComponent } from "@health-frontend/app/components/sse-client/sse-client.component";
 import { SseSignalComponent } from "@health-frontend/app/components/sse-signal/sse-signal.component";
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, SseClientComponent, SseSignalComponent],
+  imports: [NxWelcomeComponent, RouterModule, SseSignalComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
